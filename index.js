c67f0049de5577624e477edb7c9c057aae65a51c
Ramilbey <ramilbey505711503@gmail.com>
2025-06-07 22:50:44 +0800

    disconnect, port

--- index.js
+++ index.js
@@ -6,12 +6,20 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-io.on('connection', (socket) => {
-    console.log('A user connected');
+io.on("connection", (socket) => {
+  console.log("A user connected");
 
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
-    })
-})
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
