893670d792068d91810ad6fd910a004c90d978bd
Ramilbey <ramilbey505711503@gmail.com>
2025-06-07 22:48:22 +0800

    send to all clients

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server)
+const io = socketIo(server);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    })
+})
